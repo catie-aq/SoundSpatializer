5ce2e8bdbd7900029b6d3fa69f276e509586fd3b
Jeremy Laviole <j.laviole@catie.fr>
2021-05-24 13:58:16 +0200

    First commit - working version, no sound added in the repo.

---
+++ Assets/SoundPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+
+    private AudioSource source;
+    private Transform CameraTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        CameraTransform = GameObject.Find("Main Camera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(this.transform.position, CameraTransform.position);
+
+       // Debug.Log("Distance: " + dist);
+      
+    }
+}
